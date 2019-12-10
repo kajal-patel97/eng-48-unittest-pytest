1e5cb0ad27631225bad833d503b85b2c1528719e
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-10 10:09:52 +0000

    tests passed

---
+++ simple_calc.py
@@ -0,0 +1,13 @@
+class SimpleCalc():
+
+    def add(self, arg1, arg2):
+        return 6
+
+    def subtract(self, arg1, arg2):
+        return 2
+
+    def multiply(self, arg1, arg2):
+        return 6
+
+    def divide(self, arg1, arg2):
+        return 2
