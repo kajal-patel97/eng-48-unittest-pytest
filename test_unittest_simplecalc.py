3bd8b6f5a03ef5d5f796a5afe6b85c754c0a55f5
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-10 10:16:23 +0000

    new tests passed

--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -7,13 +7,18 @@ class Calcetest(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(2, 4), 6)
+        self.assertEqual(self.calc.add(4, 4), 8)
+
 
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(5, 2), 3)
 
     def test_muliply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(10, 3), 30)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(8, 4), 2)
+        self.assertEqual(self.calc.divide(30, 5), 6)
 
